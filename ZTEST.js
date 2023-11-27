3a432bd5a33ab5e45469428845e521a4ebf12676
vibhukumar2002 <140388278+vibhukumar2002@users.noreply.github.com>
2023-11-27 10:23:43 +0530

    first commit

---
+++ ZTEST.js
@@ -0,0 +1,13 @@
+window.onload=()=>
+{
+    var fromcur=document.querySelector("#fromcur")
+    var tocur=document.querySelector("#tocur")
+    var calcbtn=document.querySelector("#calc")
+    var resultdiv=document.querySelector("#result")
+    var symbols =document.querySelector("#symbols")
+    var amount=document.querySelector("#amount")
+    calcbtn.onclick=()=>{
+        console.log(amount.value)
+       
+    }
+}
